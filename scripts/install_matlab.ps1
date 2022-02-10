839136e080334586fe2d8d28343e4b0faa73b040
Raluca Chis <raluca.chis@analog.com>
2022-02-10 13:03:37 +0200

    Add 'wait' option for silent install
    Signed-off-by: Raluca Chis <raluca.chis@analog.com>

--- scripts/install_matlab.ps1
+++ scripts/install_matlab.ps1
@@ -1,2 +1,5 @@
-D:\\setup.exe -inputFile C:\\USERS\\VAGRANT\\DOCUMENTS\\matlab-files\\installer_input.txt
-Start-Sleep -s 1800
+Write-Output "Start MATLAB installation."
+
+Start-Process -FilePath D:\\setup.exe -Args "-inputFile C:\\USERS\\VAGRANT\\DOCUMENTS\\matlab-files\\installer_input.txt" -Verb RunAs -Wait
+
+Get-Content C:\\USERS\\VAGRANT\\DOCUMENTS\\matlab-files\\mathworks.log
--- scripts/install_vivado.ps1
+++ scripts/install_vivado.ps1
@@ -1,14 +1,23 @@
 Start-Process -FilePath C:\\USERS\\VAGRANT\\DOCUMENTS\\vivado-files\\7z2107-x64.exe -Args "/S" -Verb RunAs -Wait
-Start-Sleep -s 30
-setx PATH "C:\Program Files\7-Zip;%PATH%"
+Write-Output "7zip installed."
 
-7z.exe x C:\\USERS\\VAGRANT\\DOCUMENTS\\vivado-files\\Xilinx_Unified_2021.1_0610_2318_Win64.exe -oC:\\USERS\\VAGRANT\\DOCUMENTS\\vivado\\
+Write-Output "Add 7zip to PATH environment variable."
+Start-Process -FilePath C:\\Windows\\System32\\setx.exe -Args "PATH 'C:\Program Files\7-Zip;%PATH%'" -Verb RunAs -Wait
+Write-Output "7zip added to PATH environment variable."
 
+Write-Output "Extract .exe file."
+Start-Process -FilePath "C:\\Program Files\\7-Zip\\7z.exe" -Args "x C:\\USERS\\VAGRANT\\DOCUMENTS\\vivado-files\\Xilinx_Unified_2021.1_0610_2318_Win64.exe -o C:\\USERS\\VAGRANT\\DOCUMENTS\\vivado\\" -Verb RunAs -Wait
+Write-Output ".exe file extracted."
+
+Write-Output "Copy configuration file and authentication key."
 Copy-Item "C:\\USERS\\VAGRANT\\DOCUMENTS\\vivado-files\\install_config.txt" -Destination "C:\\USERS\\VAGRANT\\DOCUMENTS\\vivado\\bin\\"
 Copy-Item "C:\\USERS\\VAGRANT\\DOCUMENTS\\vivado-files\\wi_authentication_key" -Destination "C:\\USERS\\VAGRANT\\DOCUMENTS\\vivado\\bin\\"
+Write-Output "Files copied."
 
-cd C:\\USERS\\VAGRANT\\DOCUMENTS\\vivado\\bin\\
-.\xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config install_config.txt
+Write-Output "Start Vivado installation."
+Start-Process -FilePath 'C:\Windows\System32\cmd.exe' -Args "C:\\USERS\\VAGRANT\\DOCUMENTS\\vivado\\bin\\xsetup.bat --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config install_config.txt" -Verb RunAs -Wait
+Write-Output "Vivado installed."
 
-setx PATH "C:\Xilinx\Vivado\2021.1\bin;%PATH%"
-
+Write-Output "Add Vivado to PATH environment variable."
+Start-Process -FilePath C:\\Windows\\System32\\setx.exe -Args "PATH 'C:\Xilinx\Vivado\2021.1\bin;%PATH%'" -Verb RunAs -Wait
+Write-Output "Vivado added to PATH environment variable."
