f404d3ea2f29cd563758c603c09a87545d1ef98c
Raluca Chis <raluca.chis@analog.com>
2023-01-11 17:03:02 +0200

    Fix dis-updates.bat
    Signed-off-by: Raluca Chis <raluca.chis@analog.com>

--- scripts/dis-updates.bat
+++ scripts/dis-updates.bat
@@ -1,4 +1,4 @@
-rem http://www.windows-commandline.com/disable-automatic-updates-command-line/
+rem https://www.windows-commandline.com/disable-automatic-updates-command-line/
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
 
 rem remove optional WSUS server settings
